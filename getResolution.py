9df82cde2364729884951f9cc9e201a77d8a2310
Blake Huber <blakeca00@gmail.com>
2020-06-05 01:13:47 -0500

    getResolution.py: modified for python3 from python2

--- getResolution.py
+++ getResolution.py
@@ -1,29 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-# modified version of getMonitorInfo.py 
+# modified version of getMonitorInfo.py
 # (last line changed "current" monitor info
 # change to enable start_conky_laptop.sh capabilities
 
-import gtk
+import tkinter as tk
 
+root = tk.Tk()
 
-window = gtk.Window()
+cols = root.winfo_screenwidth()
+rows = root.winfo_screenheight()
+
 
 # the screen contains all monitors
-screen = window.get_screen()
-print "screen size: %d x %d" % (gtk.gdk.screen_width(),gtk.gdk.screen_height())
+print("screen size: %d x %d" % (rows, cols))
 
-# collect data about each monitor
-monitors = []
-nmons = screen.get_n_monitors()
-print "there are %d monitors" % nmons
-for m in range(nmons):
-  mg = screen.get_monitor_geometry(m)
-  print "monitor %d: %d x %d" % (m,mg.width,mg.height)
-  monitors.append(mg)
 
 # current monitor
-curmon = screen.get_monitor_at_window(screen.get_active_window())
-x, y, width, height = monitors[curmon]
-#print "monitor %d: %d x %d (current)" % (curmon,width,height) 
-print "%d x %d (current)" % (width,height) 
+print("%d x %d (current)" % (cols, rows))
